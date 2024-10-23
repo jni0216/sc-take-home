9a591fb6bb38a788b3dd74e2d248508222e120f5
Jason Ni <160880310+jni0216@users.noreply.github.com>
2024-10-23 16:43:05 +1100

    Move Folder - Completed Implementation

--- sc-take-home-assessment-take-home-2025/folder/move_folder.go
+++ sc-take-home-assessment-take-home-2025/folder/move_folder.go
@@ -1,7 +1,62 @@
 package folder
 
-func (f *driver) MoveFolder(name string, dst string) ([]Folder, error) {
-	// Your code here...
+import (
+	"errors"
+	"strings"
+)
 
-	return []Folder{}, nil
+// Approach and reasoning
+//
+// We will take a faster implementation using 2 passes to implement move folder
+// due to typically requiring fast desired behaviour when moving things.
+//
+// Unaccounted for edge cases missing in specification
+//
+// No way to differentiate if folders have the same name in different locations.
+// Can't move a folder to the root node (start of an organization)
+//
+func (f *driver) MoveFolder(name string, dst string) ([]Folder, error) {
+	folders := f.folders
+
+	// Get the folders and perform error checking
+	var sourceFolder *Folder
+	var destinationFolder *Folder
+
+	for i := range folders {
+		if folders[i].Name == name {
+			sourceFolder = &folders[i]
+		}
+		if folders[i].Name == dst {
+			destinationFolder = &folders[i]
+		}
+	}
+
+	if sourceFolder == nil {
+		return nil, errors.New("source folder does not exist")
+	}
+	if destinationFolder == nil {
+		return nil, errors.New("destination folder does not exist")
+	}
+	if sourceFolder == destinationFolder {
+		return nil, errors.New("cannot move a folder to itself")
+	}
+	if sourceFolder.OrgId != destinationFolder.OrgId {
+		return nil, errors.New("cannot move a folder to a different organization")
+	}
+	if strings.HasPrefix(destinationFolder.Paths, sourceFolder.Paths) {
+		return nil, errors.New("cannot move a folder to a child of itself")
+	}
+
+	// For all folders, if it is the source folder (e.g. a.b), or a child of the source folder (e.g. a.b...)
+	// Replace prefix with destination folder path (e.g. c.x.b) then append the rest of the path (e.g. c.x.b...)
+	oldPathPrefix := sourceFolder.Paths
+	newPathPrefix := destinationFolder.Paths + "." + sourceFolder.Name
+
+	for i := range folders {
+		if strings.HasPrefix(folders[i].Paths, oldPathPrefix) {
+			folders[i].Paths = newPathPrefix + strings.TrimPrefix(folders[i].Paths, oldPathPrefix)
+		}
+	}
+
+	return folders, nil
 }
